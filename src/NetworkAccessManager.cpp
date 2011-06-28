5e7ee949a82a9c511c9719138ee8205e9772f334
shogo-ohta <shogo-ohta@cookpad.com>
2011-06-28 15:28:26 +0900

    prevent request google ads

--- src/NetworkAccessManager.cpp
+++ src/NetworkAccessManager.cpp
@@ -2,18 +2,21 @@
 #include "WebPage.h"
 #include <iostream>
 
-
 NetworkAccessManager::NetworkAccessManager(QObject *parent):QNetworkAccessManager(parent) {
 }
 
 QNetworkReply* NetworkAccessManager::createRequest(QNetworkAccessManager::Operation oparation, const QNetworkRequest &request, QIODevice * outgoingData = 0) {
-  QNetworkRequest new_request(request);
-  QHashIterator<QString, QString> item(m_headers);
-  while (item.hasNext()) {
+  if ("googleads.g.doubleclick.net" == request.url().host() || "pagead2.googlesyndication.com" == request.url().host()) {
+    return QNetworkAccessManager::createRequest(oparation, QNetworkRequest(QUrl()));
+  } else {
+    QNetworkRequest new_request(request);
+    QHashIterator<QString, QString> item(m_headers);
+    while (item.hasNext()) {
       item.next();
       new_request.setRawHeader(item.key().toAscii(), item.value().toAscii());
+    }
+    return QNetworkAccessManager::createRequest(oparation, new_request, outgoingData);
   }
-  return QNetworkAccessManager::createRequest(oparation, new_request, outgoingData);
 };
 
 void NetworkAccessManager::addHeader(QString key, QString value) {
